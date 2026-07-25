6d9d8e90cc8cd8aaaab19a7f127a8f7ab3538714
SajaAsfour <sajanazih2004@gmail.com>
2026-07-25 17:26:48 +0300

    feat: add inital (not the full version) account authentication endpoints

--- ElectronicLibrary.PL/Controllers/AccountController.cs
+++ ElectronicLibrary.PL/Controllers/AccountController.cs
@@ -1,11 +1,72 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ElectronicLibrary.BLL.Interfaces.Authentication;
+using ElectronicLibrary.DAL.DTOs.Requests.Authentication;
+using ElectronicLibrary.DAL.DTOs.Responses.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
-namespace ElectronicLibrary.PL.Controllers
+namespace ElectronicLibrary.PL.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
+    private readonly IAuthenticationService _authenticationService;
+
+    public AccountController(
+        IAuthenticationService authenticationService)
     {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost("register")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(AuthenticationResponse),StatusCodes.Status200OK)]
+    [ProducesResponseType( StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<AuthenticationResponse>> Register(RegisterRequest request)
+    {
+        var response = await _authenticationService.RegisterAsync(request);
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(AuthenticationResponse),StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<AuthenticationResponse>> Login(LoginRequest request)
+    {
+        var response =await _authenticationService.LoginAsync(request);
+
+        return Ok(response);
+    }
+
+    [HttpPost("refresh-token")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(AuthenticationResponse),StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<AuthenticationResponse>>RefreshToken(RefreshTokenRequest request)
+    {
+        var response = await _authenticationService.RefreshTokenAsync(request);
+
+        return Ok(response);
+    }
+
+    [HttpPost("logout")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Logout()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            return Unauthorized();
+        }
+
+        await _authenticationService.LogoutAsync(userId);
+
+        return NoContent();
     }
 }
