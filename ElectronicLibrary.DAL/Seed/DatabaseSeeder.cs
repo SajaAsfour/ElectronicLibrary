a2cfdce1bab4d3134d992d7ab31b0849f5057192
SajaAsfour <sajanazih2004@gmail.com>
2026-07-25 15:34:08 +0300

    feat: seed default roles and admin user

--- ElectronicLibrary.DAL/Seed/DatabaseSeeder.cs
+++ ElectronicLibrary.DAL/Seed/DatabaseSeeder.cs
@@ -1,12 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ElectronicLibrary.DAL.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace ElectronicLibrary.DAL.Seed
+namespace ElectronicLibrary.DAL.Seed;
+
+public static class DatabaseSeeder
 {
-    internal class DatabaseSeeder
+    private static readonly string[] Roles =
+    [
+        "Admin",
+        "Customer",
+        "Seller"
+    ];
+
+    public static async Task SeedAsync(
+        IServiceProvider services,
+        IConfiguration configuration)
     {
+        using var scope = services.CreateScope();
+
+        var roleManager =
+            scope.ServiceProvider
+                .GetRequiredService<
+                    RoleManager<IdentityRole>>();
+
+        var userManager =
+            scope.ServiceProvider
+                .GetRequiredService<
+                    UserManager<ApplicationUser>>();
+
+        foreach (var roleName in Roles)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(
+                    new IdentityRole(roleName));
+            }
+        }
+
+        var adminEmail =
+            configuration["SeedAdmin:Email"];
+
+        var adminPassword =
+            configuration["SeedAdmin:Password"];
+
+        if (string.IsNullOrWhiteSpace(adminEmail) ||
+            string.IsNullOrWhiteSpace(adminPassword))
+        {
+            return;
+        }
+
+        var admin =
+            await userManager.FindByEmailAsync(
+                adminEmail);
+
+        if (admin is null)
+        {
+            admin = new ApplicationUser
+            {
+                FullName = "System Administrator",
+                UserName = adminEmail,
+                Email = adminEmail,
+                EmailConfirmed = true
+            };
+
+            var createResult =
+                await userManager.CreateAsync(
+                    admin,
+                    adminPassword);
+
+            if (!createResult.Succeeded)
+            {
+                var errors =
+                    string.Join(
+                        ", ",
+                        createResult.Errors.Select(
+                            x => x.Description));
+
+                throw new InvalidOperationException(
+                    errors);
+            }
+        }
+
+        if (!await userManager.IsInRoleAsync(
+                admin,
+                "Admin"))
+        {
+            await userManager.AddToRoleAsync(
+                admin,
+                "Admin");
+        }
     }
 }
--- ElectronicLibrary.PL/Program.cs
+++ ElectronicLibrary.PL/Program.cs
@@ -1,37 +1,31 @@
-
+using ElectronicLibrary.DAL.Seed;
 using ElectronicLibrary.PL.Extensions;
 
-namespace ElectronicLibrary.PL
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            builder.Services.AddDatabaseServices(builder.Configuration);
+builder.Services.AddDatabaseServices(
+    builder.Configuration);
 
-            var app = builder.Build();
+builder.Services.AddIdentityServices();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
+builder.Services.AddJwtAuthenticationServices(
+    builder.Configuration);
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+var app = builder.Build();
 
 
-            app.MapControllers();
+app.UseHttpsRedirection();
 
-            app.Run();
-        }
-    }
-}
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DatabaseSeeder.SeedAsync(
+    app.Services,
+    app.Configuration);
+
+app.Run();
