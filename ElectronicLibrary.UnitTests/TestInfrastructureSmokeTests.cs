6f4ce0d0d412acb4e7262cc190e94976b540e166
SajaAsfour <sajanazih2004@gmail.com>
2026-08-03 17:37:11 +0300

    test: add unit and integration test foundations

---
+++ ElectronicLibrary.UnitTests/TestInfrastructureSmokeTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace ElectronicLibrary.UnitTests;
+
+public class TestInfrastructureSmokeTests
+{
+    [Fact]
+    public void UnitTestProject_CanLoadBusinessLayer()
+    {
+        Assembly assembly = Assembly.Load("ElectronicLibrary.BLL");
+
+        Assert.Equal("ElectronicLibrary.BLL", assembly.GetName().Name);
+    }
+}
